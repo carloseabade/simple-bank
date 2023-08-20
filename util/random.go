3da455713970eda6d6c43a36dfe7dd21510791b2
Carlos Abade <carloseabade@gmail.com>
2023-08-20 12:17:29 -0300

    fix: remove comments

--- util/random.go
+++ util/random.go
@@ -12,12 +12,10 @@ func init() {
   rand.Seed(time.Now().UnixNano())
 }
 
-// RandomInt generates a random integer between min and max
 func RandomInt(min, max int64) int64 {
   return min + rand.Int63n(max - min + 1)
 }
 
-// RandomString generates a random string of length n
 func RandomString(n int) string {
   var sb strings.Builder
   k := len(alphabet)
@@ -30,24 +28,20 @@ func RandomString(n int) string {
   return sb.String()
 }
 
-// RandomOwner generates a random owner name
 func RandomOwner() string {
   return RandomString(6)
 }
 
-// RandomMoney generates a random amount of money
 func RandomMoney() int64 {
   return RandomInt(0, 1000)
 }
 
-// RandomCurrency generates a random currency code
 func RandomCurrency() string {
   currencies := []string{"EUR", "USD", "CAD"}
   n := len(currencies)
   return currencies[rand.Intn(n)]
 }
 
-// RandomAmmount generates a random amount for entry
 func RandomAmmount() int64 {
   return RandomInt(-100, 100)
 }
