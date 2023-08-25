40af97480322211ad2ba90092d6e7a53b5b2525b
Carlos Abade <carloseabade@gmail.com>
2023-08-24 22:39:17 -0300

    feat: add config support with viper

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
   "database/sql"
+  "fmt"
   "log"
+
+  "github.com/carloseabade/simple-bank/util"
 
   _ "github.com/lib/pq"
   "github.com/carloseabade/simple-bank/api"
   db "github.com/carloseabade/simple-bank/db/sqlc"
 )
 
-const (
-  dbDriver = "postgres"
-  dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-  serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-  conn, err := sql.Open(dbDriver, dbSource)
+  config, err := util.LoadConfig(".")
+  if err != nil {
+    log.Fatal("cannot load config:", err)
+  }
+  fmt.Println(config)
+  conn, err := sql.Open(config.DBDriver, config.DBSource)
   if err != nil {
     log.Fatal("cannot connect to db:", err)
   }
@@ -24,7 +26,7 @@ func main() {
   store := db.NewStore(conn)
   server := api.NewServer(store)
 
-  err = server.Start(serverAddress)
+  err = server.Start(config.ServerAddress)
   if err != nil {
     log.Fatal("cannot start server:", err)
   }
